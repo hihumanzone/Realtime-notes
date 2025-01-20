3f3bbfc3106c810a5b6ec66a7694d7e55a16d656
emoo man <101509991+hihumanzone@users.noreply.github.com>
2025-01-20 16:08:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -123,5 +123,4 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+module.exports = server;
